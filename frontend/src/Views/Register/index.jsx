31f8b8ff0b4647295221fdeacdb99a4a7ed33d58
meQlause <me@ardial.tech>
2025-08-04 14:52:07 +0700

    feat(frontend): update index.html, add Tailwind CSS configuration, and enhance LoginPage component
    - Updated index.html to include Google Fonts for improved typography.
    - Added tailwind.config.js for Tailwind CSS configuration, defining custom font families.
    - Enhanced LoginPage component with animations using framer-motion and improved layout.
    - Included a new image asset for the LoginPage.
    - Updated package.json to include new dependencies: framer-motion and lucide-react.

--- frontend/src/Views/Register/index.jsx
+++ frontend/src/Views/Register/index.jsx
@@ -1,12 +1,12 @@
-import AuthLayout from "../../Layouts/AuthLayout"
+import AuthLayout from "../../Layouts/AuthLayout";
 
 const Register = () => {
   return (
-    <AuthLayout>
+    <>
       <h1>Register</h1>
       <h2>Test</h2>
-    </AuthLayout>
-  )
-}
+    </>
+  );
+};
 
-export default Register
+export default Register;
