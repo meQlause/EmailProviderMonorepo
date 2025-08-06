93acd5af90f1741a54a577f6b5cb45cdec43a416
Fahrulmosaja <fachrulmosaja@gmail.com>
2025-08-06 20:16:29 +0800

    feat(frontend): fix import RegisterForm

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import AuthLayout from "../Components/layouts/AuthLayout/index.jsx";
-import RegisterForm from "../Components/views/Auth/Register/Index.jsx.jsx";
+import RegisterForm from "../Components/views/Auth/Register/Index.jsx";
 import PageHead from "../Components/commons/PageHead.jsx";
 import RightPanel from "../Components/views/Auth/Login/RightPanel.jsx";
 
